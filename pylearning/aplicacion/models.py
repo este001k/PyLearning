d29ae66314dbfa0cc6fe912857462ccb69f38185
este001k <este.nunez@duocuc.cl>
2024-04-01 15:07:17 -0300

    Creacion del proyecto

---
+++ pylearning/aplicacion/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Usuario(models.Model):
+
+    nombre_usuario = models.CharField(max_length=30, unique=True)
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
